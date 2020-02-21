ae5d140cddc72f742471033a8ca03495af0ed2c2
Simon Shine <shreddedglory@gmail.com>
2020-02-21 03:36:42 +0100

    Use heterocephalus + blaze as template engine
    Use my homepage as root page, but with list of blog posts.

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import           RIO
-import           RIO.FilePath ((</>), isExtensionOf)
+import           RIO.FilePath ((</>), (<.>), isExtensionOf)
 import qualified RIO.Map as Map
 import           RIO.Map (Map)
 import qualified RIO.Text as Text
 import           RIO.Text (Text)
-import           RIO.Time (UTCTime)
+import           RIO.Text.Lazy (toStrict)
 
 import Data.Text.IO (putStrLn)
 
-import CMarkGFM (commonmarkToHtml)
-
 import Web.Spock
 import Web.Spock.Config
+
+import Text.Heterocephalus (compileHtmlFile)
+import Text.Blaze (Markup)
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 import System.Directory (listDirectory)
 import System.FSNotify
@@ -42,36 +45,24 @@ main = do
 
 app :: SpockM () AppSession AppState ()
 app = do
-  get root postsOverview
-  get ("posts" <//> var) postView
+  get root rootPage
+  get ("posts" <//> var) viewBlogPostPage
 
-postView :: Text -> HaskPressM a
-postView (Text.unpack -> name) = do
-  blogPost <- lookupBlogPost (postsDir </> name)
+rootPage :: HaskPressM a
+rootPage = do
+  posts <- getBlogPosts
+  html . toStrict . renderHtml . rootTemplate $ posts
+
+rootTemplate :: Map FilePath BlogPost -> Markup
+rootTemplate blogPosts =
+  $(compileHtmlFile "templates/index.html.tmpl")
+
+viewBlogPostPage :: Text -> HaskPressM a
+viewBlogPostPage (Text.unpack -> name) = do
+  blogPost <- lookupBlogPost (postsDir </> name <.> "md")
   case blogPost of
     Nothing -> text $ "Post not found!"
     Just content -> html (blogPostContentRendered content)
-
-postsOverview :: HaskPressM a
-postsOverview = do
-  posts <- getBlogPosts
-  html $ mconcat
-    [ "<h1>Blog posts</h1>"
-    , htmlBlogPosts posts
-    ]
-
-htmlBlogPosts :: Map FilePath BlogPost -> Text
-htmlBlogPosts =
-  ("<ul>" <>) . (<> "</ul>") . foldMap f . Map.toList
-  where
-    f :: (FilePath, BlogPost) -> Text
-    f (filePath, blogPost @ BlogPost{..}) = mconcat
-      [ "<li><a href=\"/", Text.pack filePath, "\">"
-      , blogPostTitle
-      , "</a> "
-      , showBlogPostDate blogPostDate
-      , "</li>"
-      ]
 
 getBlogPosts :: HaskPressM (Map FilePath BlogPost)
 getBlogPosts = do
@@ -80,14 +71,6 @@ getBlogPosts = do
 
 lookupBlogPost :: FilePath -> HaskPressM (Maybe BlogPost)
 lookupBlogPost name = Map.lookup name <$> getBlogPosts
-
-readBlogPost :: MonadIO m => FilePath -> m BlogPost
-readBlogPost filePath = do
-  blogPostContent <- readFileUtf8 filePath
-  let blogPostDate = readDateFromFilePath filePath
-      blogPostTitle = readTitleFromContent blogPostContent
-      blogPostContentRendered = commonmarkToHtml [] [] blogPostContent
-  pure BlogPost{..}
 
 readBlogPosts :: MonadIO m => m (Map FilePath BlogPost)
 readBlogPosts = do
--- src/Web/HaskPress/BlogPost.hs
+++ src/Web/HaskPress/BlogPost.hs
@@ -3,26 +3,50 @@ module Web.HaskPress.BlogPost where
 
 import           RIO
 import           RIO.List
-import qualified RIO.Map as Map
-import           RIO.Map (Map)
 import qualified RIO.Text as Text
 import           RIO.Text (Text)
 import           RIO.Time (UTCTime)
 import           Data.Time.Format (parseTimeM, defaultTimeLocale, formatTime)
+import           RIO.FilePath (takeBaseName)
+
+import CMarkGFM (commonmarkToHtml)
+
+import qualified Text.Blaze as Blaze
+import           Text.Blaze (ToMarkup)
 
 type BlogTitle = Text
+newtype BlogDate = BlogDate { unBlogDate :: Maybe UTCTime }
+  deriving (Eq, Ord, Show)
+
 data BlogPost = BlogPost
-  { blogPostDate            :: Maybe UTCTime
+  { blogPostSlug            :: Text
+  , blogPostDate            :: BlogDate
   , blogPostTitle           :: BlogTitle
   , blogPostContent         :: Text
   , blogPostContentRendered :: Text
   } deriving (Eq, Ord, Show)
 
-readDateFromFilePath :: FilePath -> Maybe UTCTime
-readDateFromFilePath =
+instance ToMarkup BlogDate where
+  toMarkup (BlogDate Nothing) = Blaze.text ""
+  toMarkup (BlogDate (Just date)) =
+    Blaze.text . Text.pack . formatTime defaultTimeLocale "%Y-%m-%d" $ date
+
+readBlogPost :: MonadIO m => FilePath -> m BlogPost
+readBlogPost filePath = do
+  blogPostContent <- readFileUtf8 filePath
+  let blogPostSlug = Text.pack (takeBaseName filePath)
+      blogPostDate = readDateFromSlug blogPostSlug
+      blogPostTitle = readTitleFromContent blogPostContent
+      blogPostContentRendered = commonmarkToHtml [] [] blogPostContent
+  pure BlogPost{..}
+
+readDateFromSlug :: Text -> BlogDate
+readDateFromSlug =
+  BlogDate .
   parseTimeM allowSpace defaultTimeLocale "%Y-%m-%d" .
-  concat . intersperse "-" . take 3 . splitOn "-"
-  where allowSpace = True
+  Text.unpack . Text.intercalate "-" . take 3 . Text.split (== '-')
+  where
+    allowSpace = True
 
 showBlogPostDate :: Maybe UTCTime -> Text
 showBlogPostDate Nothing = ""
@@ -37,9 +61,3 @@ readTitleFromContent =
   . Text.lines
   where
     isHeader c = c == ' ' || c == '#'
-
-splitOn :: Eq a => [a] -> [a] -> [[a]]
-splitOn _sep [] = []
-splitOn seps xs = case span (`notElem` seps) xs of
-  (ys, []) -> [ys]
-  (ys, rest) -> ys : splitOn seps (drop 1 rest)
